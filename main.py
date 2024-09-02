0d712d6a6cac942138f5b34b428d9767035f8468
Dmitry Shvydenko <rapax.deus@gmail.com>
2024-09-02 22:35:16 +0300

    added

--- main.py
+++ main.py
@@ -1,38 +1,15 @@
-class Player:
-    max_health = 100
-
-    def __init__(self, health):
-        self.health = health
-    def check_status(self):
-        if self.health <= 0:
-            print('end game')
-        print(self.health)
-
-class Weapon:
-    def __init__(self, name, damage):
-        self.name = name
-        self.damage = damage
-    def attack(self, hero):
-        self.damage - hero.health
-
+from hero import *
 
 def main():
-    # weapon
-    sword = Weapon('Sword', 5)
-    axe = Weapon('Axe', 30)
+    # hero
+    hero = Hero('KIT', 100)
+    hero.status()
 
-    # hero 1
-    hero = Player(100)
-    hero.check_status()
+    # Enum
+    goblin = Enum('Goblin', 20)
+    goblin.status()
 
-    # monster
-    goblin = Player(10)
-    goblin.check_status()
-
-    #
-    axe.attack(goblin)
-    goblin.check_status()
-
+    hero.attack_enum(goblin)
 
 if __name__ == '__main__':
     main()
